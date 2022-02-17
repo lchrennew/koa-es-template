5b571add13f8df1e4d5f144990e532d087ebbb1b
lichun <lchrennew@126.com>
2022-02-18 00:09:52 +0800

    opts: refactor

--- src/web/routes/controller.js
+++ src/web/routes/controller.js
@@ -5,6 +5,10 @@ import Router from '@koa/router'
  * Base class for Controllers to standardize binding to express Router.
  */
 class Controller {
+    logger
+    router;
+    eventBus;
+
     constructor(config) {
         this.router = new Router().prefix(config.baseUriPath)
         this.config = config;
@@ -12,51 +16,47 @@ class Controller {
         this.eventBus = config.eventBus
     }
 
-    logger
-    router;
-    eventBus;
-
-    get(path, handlers, permission) {
-        const handler = handlers?.map?.(h => h?.bind(this)) ?? [handlers?.bind(this)]
+    get(path, handlers,) {
+        const handler = handlers?.map?.(h => h?.bind(this)) ?? [ handlers?.bind(this) ]
         handler && this.router.get(
             path,
             ...handler
         );
     }
 
-    post(path, handlers, permission) {
-        const handler = handlers?.map?.(h => h?.bind(this)) ?? [handlers?.bind(this)]
+    post(path, handlers,) {
+        const handler = handlers?.map?.(h => h?.bind(this)) ?? [ handlers?.bind(this) ]
         handler && this.router.post(
             path,
             ...handler
         );
     }
 
-    put(path, handlers, permission) {
-        const handler = handlers?.map?.(h => h?.bind(this)) ?? [handlers?.bind(this)]
+    put(path, handlers,) {
+        const handler = handlers?.map?.(h => h?.bind(this)) ?? [ handlers?.bind(this) ]
         handler && this.router.put(
             path,
             ...handler,
         );
     }
 
-    delete(path, handlers, permission) {
-        const handler = handlers?.map?.(h => h?.bind(this)) ?? [handlers?.bind(this)]
+    delete(path, handlers,) {
+        const handler = handlers?.map?.(h => h?.bind(this)) ?? [ handlers?.bind(this) ]
         handler && this.router.delete(
             path,
             ...handler,
         );
     }
 
-    patch(path, handlers, permission) {
-        const handler = handlers?.map?.(h => h?.bind(this)) ?? [handlers?.bind(this)]
+    patch(path, handlers,) {
+        const handler = handlers?.map?.(h => h?.bind(this)) ?? [ handlers?.bind(this) ]
         handler && this.router.patch(
             path,
             ...handler,
         );
     }
 
-    upload(path, filehandler, handler, permission) {
+    upload(path, filehandler, handler,) {
         this.router.post(
             path,
             filehandler,
