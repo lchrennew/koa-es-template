98d5d7815f3192acdd98ce65fabb1a100d15b4f6
lichun <lchrennew@126.com>
2022-09-22 14:15:37 +0800

    v1.0.24

---
+++ src/web/server2.js
@@ -0,0 +1,48 @@
+import EventEmitter from 'eventemitter2';
+import getApp from './app.js';
+import { createOptions } from './options.js';
+
+const createApp = async options => {
+    const logger = options.getLogger('server.js');
+    const eventBus = new EventEmitter.EventEmitter2();
+
+    const config = {
+        eventBus,
+        ...options,
+    };
+
+    const app = getApp(config);
+
+
+    return new Promise((resolve, reject) => {
+        const payload = {
+            app,
+            config,
+            eventBus,
+        };
+
+        if (options.start) {
+            const server = app.listen(options.listen, () =>
+                logger.info(`${process.env.SERVER_NAME} has started.`, server.address()),
+            );
+            server.on('listening', () => resolve({ ...payload, server }));
+            server.on('error', reject);
+        } else {
+            resolve({ ...payload });
+        }
+    });
+};
+
+/**
+ *
+ * @param opts {{index:Controller, preHook, preRouterHook, baseUriPath, listen }}
+ * @return {Promise<unknown>}
+ */
+export const start = async opts => createApp(createOptions({ start: true, ...opts }));
+
+/**
+ *
+ * @param opts {{index:Controller, preHook, preRouterHook, baseUriPath, listen }}
+ * @return {Promise<unknown>}
+ */
+export const create = async opts => createApp(createOptions({ start: false, ...opts }));
