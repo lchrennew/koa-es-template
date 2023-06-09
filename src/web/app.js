bfb8368b37c9e7000953760b1e64bbf1bd7f26a8
lichun <lchrennew@126.com>
2023-06-10 07:37:02 +0800

    opt: update koa-error to es-koa-error

--- src/web/app.js
+++ src/web/app.js
@@ -2,7 +2,7 @@ import cors from '@koa/cors'
 import Koa from 'koa'
 import body from 'es-koa-body';
 import compress from 'koa-compress'
-import error from 'koa-error';
+import error from 'es-koa-error';
 import requestLogger from './middlewares/request-logger.js';
 import responseTime from './middlewares/response-time.js';
 import Controller from "./controller.js";
