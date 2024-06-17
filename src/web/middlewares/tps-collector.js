8d13bfc71c383a1edb3cf5d71b7017d9cd8ce0af
lichun <lchrennew@126.com>
2024-06-17 14:22:22 +0800

    opt: 调整tps口径

--- src/web/middlewares/tps-collector.js
+++ src/web/middlewares/tps-collector.js
@@ -2,23 +2,31 @@ import { getApi } from "es-fetch-api";
 import { POST } from "es-fetch-api/middlewares/methods.js";
 import { json } from "es-fetch-api/middlewares/body.js";
 
-const interval = 1000 * 60
+const second = 1000
+const minute = second * 60
 const server = process.env.SERVER_NAME
 const host = process.env.HOSTNAME
 
 export const tpsCollector = config => {
     const logger = config.getLogger('TPS_COLLECTOR');
+    const tpsBuffer = []
     const timeBox = { tps: 0 }
+
     setInterval(() => {
         const { tps } = timeBox
         timeBox.tps = 0
+        tpsBuffer.push({ tps })
+    }, second)
+
+    setInterval(() => {
+        const tps = [ ...tpsBuffer ]
+        tpsBuffer.length = 0
         const api = getApi()
         const data = { server, host, tps }
         logger.info('Collecting TPS Data: ', data)
         api(process.env.TPS_COLLECTOR, POST, json(data))
             .catch(e => logger.error(e))
-    }, interval)
-
+    }, minute)
 
     return async (ctx, next) => {
         timeBox.tps++
