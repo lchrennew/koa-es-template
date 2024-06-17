802107fd2a107c417c69a5d742a13bb80f18f00b
lichun <lchrennew@126.com>
2024-06-17 12:14:44 +0800

    opt: 增加host

--- src/web/middlewares/tps-collector.js
+++ src/web/middlewares/tps-collector.js
@@ -4,6 +4,7 @@ import { json } from "es-fetch-api/middlewares/body.js";
 
 const interval = 1000 * 60
 const server = process.env.SERVER_NAME
+const host = process.env.HOSTNAME
 
 export const tpsCollector = config => {
     const logger = config.getLogger('TPS_COLLECTOR');
@@ -12,7 +13,7 @@ export const tpsCollector = config => {
         const { tps } = timeBox
         timeBox.tps = 0
         const api = getApi()
-        const data = { server, tps }
+        const data = { server, host, tps }
         logger.info('Collecting TPS Data: ', data)
         api(process.env.TPS_COLLECTOR, POST, json(data))
             .catch(e => logger.error(e))
